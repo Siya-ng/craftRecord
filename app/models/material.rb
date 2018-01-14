551524180baae82cd2b21ca7fd5a21b4ca00006e
siya-ng <ngsiya90@gmail.com>
2018-01-14 22:09:06 +0800

    add model and controller

---
+++ app/models/material.rb
@@ -0,0 +1,3 @@
+class Material
+  include Mongoid::Document
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  get 'main/index'
+  get 'main/about'
+
+  root 'main#index'
+  get 'about' => 'main#about'
+
+
+
+  resources :plan
+  resources :material
+  resources :project
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
